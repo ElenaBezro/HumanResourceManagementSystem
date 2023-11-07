ebc806daec998714becc3acbffe7fb0965d88821
ElenaBezro <lenyka_86@mail.ru>
2023-11-07 22:44:44 +0100

    split tests with positive and negative scenaries

--- src/test/java/jobPositionManagement/JobPositionTest.java
+++ src/test/java/jobPositionManagement/JobPositionTest.java
@@ -3,25 +3,45 @@ package jobPositionManagement;
 import applicantManagement.Applicant;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class JobPositionTest {
 
     @Test
-    void isWithinBudget() {
+    void isWithinBudgetPositive() {
+        //arrange
         Applicant applicantPositive = new Applicant(80000.00);
-        Applicant applicantNegative = new Applicant(100000.00);
-        Applicant applicantNull = null;
+        JobPosition jobPosition = new JobPosition(70000.00, 90000.00);
+        //act
+        boolean ifPositive = jobPosition.isWithinBudget(applicantPositive);
+        //assert
+        assertTrue(ifPositive);
+    }
 
+    @Test
+    void isWithinBudgetNegative() {
+        //arrange
+        Applicant applicantNegative = new Applicant(100000.00);
         JobPosition jobPosition = new JobPosition(70000.00, 90000.00);
 
-        boolean ifPositive = jobPosition.isWithinBudget(applicantPositive);
+        //act
         boolean ifNegative = jobPosition.isWithinBudget(applicantNegative);
+
+        //assert
+        assertFalse(ifNegative);
+    }
+    @Test
+    void isWithinBudgetApplicantIsNull() {
+        //arrange
+        Applicant applicantNull = null;
+        JobPosition jobPosition = new JobPosition(70000.00, 90000.00);
+
+        //act
         boolean ifNull = jobPosition.isWithinBudget(applicantNull);
 
-        assertTrue(ifPositive);
-        assertFalse(ifNegative);
+        //assert
         assertFalse(ifNull);
     }
 }
--- src/test/java/recruiterManagement/RecruiterTest.java
+++ src/test/java/recruiterManagement/RecruiterTest.java
@@ -12,29 +12,48 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class RecruiterTest {
 
     @Test
-    void isSpecializedFor() {
+    void isSpecializedForPositive() {
         //arrange
         JobPosition jobPosition = new JobPosition("IT", "Backend developer");
 
         Set<String> specializedIndustriesPositive = new HashSet<>();
-        Set<String> specializedIndustriesNegative = new HashSet<>();
         specializedIndustriesPositive.add("Sales");
         specializedIndustriesPositive.add("IT");
-        specializedIndustriesNegative.add("Sales");
 
         Set<String> specializedRolesPositive = new HashSet<>();
-        Set<String> specializedRolesNegative = new HashSet<>();
         specializedRolesPositive.add("Backend developer");
         specializedRolesPositive.add("Frontend developer");
-        specializedRolesNegative.add("Frontend developer");
 
         Recruiter recruiterPositive1 = new Recruiter("Lena", specializedIndustriesPositive, specializedRolesPositive);
+
+        //act && assert
+        assertTrue(recruiterPositive1.isSpecializedFor(jobPosition));
+    }
+
+    @Test
+    void isSpecializedForNegative() {
+        //arrange
+        JobPosition jobPosition = new JobPosition("IT", "Backend developer");
+
+        Set<String> specializedIndustriesNegative = new HashSet<>();
+        specializedIndustriesNegative.add("Sales");
+
+        Set<String> specializedRolesNegative = new HashSet<>();
+        specializedRolesNegative.add("Frontend developer");
+
+        Set<String> specializedIndustriesPositive = new HashSet<>();
+        specializedIndustriesPositive.add("Sales");
+        specializedIndustriesPositive.add("IT");
+
+        Set<String> specializedRolesPositive = new HashSet<>();
+        specializedRolesPositive.add("Backend developer");
+        specializedRolesPositive.add("Frontend developer");
+
         Recruiter recruiterNegative1 = new Recruiter("Lena", specializedIndustriesPositive, specializedRolesNegative);
         Recruiter recruiterNegative2 = new Recruiter("Lena", specializedIndustriesNegative, specializedRolesPositive);
         Recruiter recruiterNegative3 = new Recruiter("Lena", specializedIndustriesNegative, specializedRolesNegative);
 
         //act && assert
-        assertTrue(recruiterPositive1.isSpecializedFor(jobPosition));
         assertFalse(recruiterNegative1.isSpecializedFor(jobPosition));
         assertFalse(recruiterNegative2.isSpecializedFor(jobPosition));
         assertFalse(recruiterNegative3.isSpecializedFor(jobPosition));
